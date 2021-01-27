9557c5eb997f47a50b5d869db9953945a2f69f11
datout <70312079+datout@users.noreply.github.com>
2021-01-27 12:39:38 +0800

    Update diy-1.sh

--- build/Lede_nanopi_r2s/diy-1.sh
+++ build/Lede_nanopi_r2s/diy-1.sh
@@ -2,7 +2,7 @@
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
 # 使用源码自带ShadowSocksR Plus+出国软件
-# sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 
 #添加自定义插件链接
 # git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
